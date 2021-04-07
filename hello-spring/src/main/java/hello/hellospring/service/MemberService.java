2ef91991f5c233f865577ce913b85b095425708e
Huhsway <seunghoe9542@naver.com>
2021-04-07 21:26:48 +0900

    회원 서비스 완료

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,23 +4,34 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-
-    // 회원가ㅣㅂ
+    // 회원가입
     public Long join(Member member){
-        // 같은 이름이 있는 중복 회원X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> { // ifPresent는 result에 값이 있으면 이라는 말말
-           throw new IllegalStateException("이미 존재하는 회원입니다.")
-        })
-
+        validateDuplicateMember(member); // 같은 이름이 있는 중복 회원X
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> { // ifPresent는 result에 값이 있으면 이라는 말 Optional을 한번 감싸면 이렇게 if문 안쓰고 ifPresent 이런걸 쓸 수 있다.
+                            throw new IllegalStateException("이미 존재하는 회원입니다.")
+                        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 
 }
