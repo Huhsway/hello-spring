ccc4375f9bac92daf5aedda9659ac57b79e8da79
Huhsway <seunghoe9542@naver.com>
2021-04-13 19:56:08 +0900

    스프링 빈과 의존관계 컴포넌트 스캔과 자동 의존관계 설정 자바 코드로 직접 스프링 빈 등록하기 회원 웹 기능-홈 화면 추가 MemberServiceTest 수정

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,19 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller // 컨트롤러 어노테이션을 쓰면 스프링 컨테이너가 컨트롤러 객체를 생성해서 넣어두고 관리함 -> 빈이 관리됨
+public class MemberController {
+
+//    private final MemberService memberService = new MemberService(); // 이렇게 생성을 필요할때 쓰는게 아니라 MemberService 내에는 별로 내용이 없으니까 스프링 컨테이너에 등록하면 하나만 등록되고 공용으로 쓰는게 낫다.
+
+    private final MemberService memberService;
+
+    @Autowired // Autowired가 있으면 스프링이 컨테이너의 멤버 서비스를 가져다 연결 시켜줌 이게바로 DI임 생성자 방식의 의존관계 주입임, setter 방식의 주입은 public으로 되있어서 누가 바꿀 수 있기 때문에 권장 x
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+}
